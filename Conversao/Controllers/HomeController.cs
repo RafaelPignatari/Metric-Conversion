64a96c79d64b5e09d53de0340434e47f93966eed
RafaelPignatari <rafaelpignatarifantinati@hotmail.com>
2021-09-07 22:53:38 -0300

    Add project files.

---
+++ Conversao/Controllers/HomeController.cs
@@ -0,0 +1,69 @@
+﻿using Conversao.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Conversao.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult converteDistancia(float numero, string medida)
+        {
+            RespostaViewModel resposta = new RespostaViewModel();
+            resposta.Converter = numero;
+            switch (medida)
+            {
+                case "cm":
+                    resposta.Index = "cm";
+                    resposta.Resultado = numero * 100;
+                    break;
+                case "mm":
+                    resposta.Index = "mm";
+                    resposta.Resultado = numero * 1000;
+                    break;
+                case "km":
+                    resposta.Index = "km";
+                    resposta.Resultado = numero / 1000;
+                    break;
+                case "pe":
+                    resposta.Index = "pe";
+                    resposta.Resultado = numero * 3.28084;
+                    break;
+                case "milha":
+                    resposta.Index = "milha";
+                    resposta.Resultado = numero * 0.000621371;
+                    break;
+                default:
+                    return View("Index", -1);
+            }
+            return View("Index", resposta);
+        }
+    }
+}
---
+++ Conversao/Models/RespostaViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Conversao.Models
+{
+    public class RespostaViewModel
+    {
+        public float Converter { get; set; }
+        public string Index { get; set; }
+        public double Resultado { get; set; }
+    }
+}
